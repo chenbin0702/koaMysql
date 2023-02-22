f23f93d1cabf87598898a5015b51a98e87b2d8bb
Lazy Boy <9229099+lazyboycb@user.noreply.gitee.com>
2023-02-22 23:37:02 +0800

    sequelize五种查询

--- src/dao/UserDao.ts
+++ src/dao/UserDao.ts
@@ -1,12 +1,56 @@
 import {model} from '../defineModel';
+import {Op,Sequelize} from 'sequelize';
+
 class UserDao
 {
- static addUser(userinfo:UserInfo)
+  static userDao:UserDao=new UserDao()
+  addUser(userinfo:UserInfo)
   {
     return model.create(userinfo)
   }
+ findUser()
+  {
+    return model.findAll({
+      raw:true,
+      // 投影查找
+      // attributes:['userid','password'] 
+    })
+  }
+  // 模糊查找
+ findByLike()
+  {
+    return model.findAll({
+      raw:true,
+      where:{
+        username:{
+          [Op.like]:'%陈'
+        }
+      }
+    })
+  }
+  // 聚合查找
+  countFind()
+  {
+    return model.findAll({
+      raw:true,
+      group:'address',
+      attributes:['address',[Sequelize.fn('count',Sequelize.col('valid')),'totalcount']],
+      where:{
+        valid:1
+      }
+    })
+  }
+  // 分页查询
+  findByWrap(pageSize:number,offset:number)
+  {
+     return model.findAll({
+      raw:true,
+      limit:pageSize,
+      offset:offset
+     })
+  }
 }
-export const {addUser}=UserDao
+export default UserDao.userDao
 export type UserInfo={
   userid:number,
   username:string,
--- src/router/user.ts
+++ src/router/user.ts
@@ -2,7 +2,7 @@ import { UserInfo } from '@/dao/UserDao';
 import {Context} from 'koa';
 import Router from 'koa-router';
 import {success} from '../common/ResResult';
-import {addUser} from '../dao/UserDao';
+import UserDao from '../dao/UserDao';
 const router=new Router()
 // 路由预处理
 router.prefix('/user')
@@ -14,8 +14,22 @@ router.get('/findUserinfo/:username',async(ctx:Context)=>{
 // 增加用户
 router.post('/addUser',async(ctx:Context)=>{
   const userinfo:UserInfo=ctx.request.body
- const dbUserInfo=await addUser(userinfo)
+ const dbUserInfo=await UserDao.addUser(userinfo)
  ctx.body=success(dbUserInfo)
 })
-
+// 获取所有用户
+router.get('/findAllUserInfo',async(ctx:Context)=>{
+  const users=await UserDao.findUser()
+  ctx.body=success(users)
+})
+// 聚合查找
+router.get('/findCount',async(ctx:Context)=>{
+  ctx.body=success(await UserDao.countFind())
+})
+// 分页查找
+router.get('/findWrapPaper/:pageNo/:pageSize',async(ctx:Context)=>{
+  const {pageNo,pageSize}=ctx.params
+  const offset=(pageNo-1)*pageSize
+  ctx.body=success(await UserDao.findByWrap(parseInt(pageSize),offset))
+})
 module.exports= router
